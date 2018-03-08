b82f8eaf6036ba4239c4ab10729fb22d8afe0064
hennin <hennin.ltn@gmail.com>
2018-03-08 20:46:45 +0900

    [add]SASS, Element-UI

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div class="header">
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: rgba(255, 255, 255, 1);
+  width: 100vw;
+  height: 20rem;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -36,5 +36,16 @@ module.exports = {
   /*
   ** Mode
   */
-  mode: "spa"
+  mode: "spa",
+  /*
+  ** Global CSS
+  */
+  css: [
+    "element-ui/lib/theme-chalk/reset.css",
+    "element-ui/lib/theme-chalk/index.css"
+  ],
+  /*
+  ** Add element-ui in our app, see plugins/element-ui.js file
+  */
+  plugins: ["@/plugins/element-ui"]
 };
